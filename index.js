b9d537fe9a45d6734f86534f22c68c42cd7a7266
LusineE <64039235+LusineE@users.noreply.github.com>
2020-07-23 01:18:19 +0400

    manr-munr changes

--- index.js
+++ index.js
@@ -1,44 +1,45 @@
 // ---------------------Factorial-------------------------------------------------------
-  let a=prompt('Ողջույն: Մուտքագրեք այն թիվը, որի ֆակտորիալը ուզում եք իմանալ:')
-  const factorial=(n)=>{
-      let fact=1
-      for (let i=2;i<=n;i++){
-          fact=fact*i
-      }
-      return fact
-  }
-  console.log (factorial(a))
+let a=prompt('Ողջույն: Մուտքագրեք այն թիվը, որի ֆակտորիալը ուզում եք իմանալ:')
+const factorial=(n)=>{
+    let fact=1
+    for (let i=2;i<=n;i++){
+        fact=fact*i
+    }
+    return fact
+}
+console.log (factorial(a))
 
 // ---------------------Stars ------------------------------------------------------
-  let b=prompt('Հիմա մուտքագրեք ցանկացած թիվ:')
-  let stars = (n) => {
-      for(let i=1;i<=n;i++){
-          let st=''
-          for(let j=1;j<=i;j++){
-              st=st+'*'
-          }
-          console.log(st)
-      }
-  }
-  console.log(stars(b))
+let b=prompt('Հիմա մուտքագրեք ցանկացած թիվ:')
+let stars = (n) => {
+    for(let i=1;i<=n;i++){
+        let st=''
+        for(let j=1;j<=i;j++){
+            st=st+'*'
+        }
+        console.log(st)
+    }
+}
+console.log(stars(b))
 
 // ---------------------Stars 1 ------------------------------------------------------
-  let c=prompt('Կրկին մուտքագրեք ցանկացած թիվ:')
-  let stars1=(n) =>{
-      for (let i=1;i<=n;i++){
-          let p=''
-          let st=''
-          for(j=1;j<=n-i;j++){
-             p=p+'-'          
-          }
-          for(k=1;k<=i;k++){
-             st=st+'*'
-          }
-          console.log(p+st)
-      }
-  }
-  console.log(stars1(c))
-//  ---------------------Stars 1 ------------------------------------------------------
+let c=prompt('Կրկին մուտքագրեք ցանկացած թիվ:')
+let stars1=(n) =>{
+    for (let i=1;i<=n;i++){
+        let p=''
+        let st=''
+        for(j=1;j<=n-i;j++){
+            p=p+'-'          
+        }
+        for(k=1;k<=i;k++){
+            st=st+'*'
+        }
+        console.log(p+st)
+    }
+}
+console.log(stars1(c))
+
+//  ---------------------Average------------------------------------------------------
 let arr=[2,3,6,10]
 const av=(n)=>{
    let sum=0
